24f9d7a2d6b3601bfa8fed980bd8ca24094eb2c5
ashwani2797 <ashwani2797@gmail.com>
2017-01-15 23:47:14 +0530

    First part complete

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app=express();
-app.get('/',function(req,res){
- res.send("hello world from server.js")
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(8085);
-console.log("running on port 8080");
+console.log("running on port 8085");
